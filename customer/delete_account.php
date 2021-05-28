f97b11437f7d6b2159f0eb142daccabfb792a524
Enabulele Jerry <jerryenabulele@gmail.com>
2021-05-28 08:41:08 +0100

    first commit

--- customer/delete_account.php
+++
@@ -1,9 +0,0 @@
-<center><!-- center begin -->
-	<h3>Do YOu Really Want To Delete Your Account?</h3>
-    
-    <form action="" method="post"><!-- form begin -->
-    	<input type="submit" name="yes" value="Yes, I Want To Delete" class="btn btn-danger">
-    	<input type="submit" name="no" value="No, I Dont Want To Delete" class="btn btn-primary">
-    </form><!-- form end -->
-
-</center><!-- center end -->
